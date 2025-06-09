e7dedcc979d53ba7e56c0cfc0997a8a6dc9c1c6e
Prasanthi-512 <jyothigandham24@gmail.com>
2025-06-09 18:29:42 +0530

    Day-12

---
+++ Day-12/test.js
@@ -0,0 +1,87 @@
+// let x = 10
+// console.log(x)
+
+
+// var x=10
+// var x=100
+// console.log(x)
+
+
+// const x=10
+// console.log(x)
+
+
+// for (let i = 1; i < 6; i++)
+//     console.log(i)
+
+// let str = ""
+// for (let i = 1; i < 6; i++)
+//     str += i + " "
+// console.log(str);
+
+
+// let x
+// x=100
+// var y=20
+// y=200
+// const z=300
+// console.log(x,y,z)
+
+// let x=10
+// var y=20
+// y=50
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+
+// let x=20
+// x += 5
+// console.log(x)
+
+// let x= 10
+// //let y=30
+// var y=30
+// //y=10
+// console.log(x == y)
+// console.log(x < y)
+// console.log(x > y)
+// console.log(x != y)
+
+
+// let a = 20
+// var b = 30
+// if (a > b){
+//     console.log("a is larger than b")
+// }
+// else if (b > a){
+//     console.log("b is larger than a")
+// }
+
+// let x = 1
+// while (5 > x){
+//     console.log("Prasanthi",x)
+//     x++
+// } 
+
+
+// let a =1
+// do{
+//     console.log(a,"Prasanthi")
+//     a++
+// }
+// while (a < 6)
+
+
+let x = true;
+let y = false;
+if (x && y) {
+  console.log("Hi");
+}
+else if (x || y){
+    console.log("Hello")
+}
+else if (! y){
+    console.log("Welcome")
+}
+
