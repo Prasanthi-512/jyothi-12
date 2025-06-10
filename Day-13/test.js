487c73d0d97a5a6141151427e34519928cfff6ec
Prasanthi-512 <jyothigandham24@gmail.com>
2025-06-10 20:07:14 +0530

    Day-13

---
+++ Day-13/test.js
@@ -0,0 +1,36 @@
+// function self (name) {
+//     return name
+// }
+// console.log("python")
+
+
+
+// const self =(name)=>{
+//     return name
+// }
+// console.log("Python")
+  
+
+// const greet = (name,Callback)=>{
+//     console.log(name);
+//     Callback();
+// }
+// greet("Pyhton",()=>console.log("How are you"))
+
+
+
+// function operate(a, b, operation) {
+//   return operation(a, b);
+// }
+
+// const add = (x, y) => x + y;
+// console.log(operate(4, 5, add)); 
+
+
+
+arr=[1,2,3,4]
+arr.map((item)=>{
+    return item+3;
+})
+console.log(arr)
+
