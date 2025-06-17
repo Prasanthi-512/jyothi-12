0d57dc2442e3d30ce8ba28724577124a2dca7c5e
Prasanthi-512 <jyothigandham24@gmail.com>
2025-06-17 19:11:24 +0530

    Using react

---
+++ day-17/src/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+// import AppHeader from './components/AppHeader';
+// import CricketScore from './components/CricketScore';
+import MovieSearch from './Components/Moviesearch';
+
+function App() {
+  return (
+    <div>
+      {/* <CricketScore target={200} totalOvers={10}  /> */}
+      <MovieSearch />
+      <br></br>
+    </div>
+  );
+}
+
+export default App;
